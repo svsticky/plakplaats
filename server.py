c4585524fdf1488e2fac7d94c9c077f8cb395917
sam32123 <samstaijen@gmail.com>
2024-03-18 09:18:05 +0100

    First commit - added comment

--- server.py
+++ server.py
@@ -324,6 +324,7 @@ def checkFileName(name):
     return newName
 
 
+# only runs when executed as script, not when used as module
 if __name__ == "__main__":
     from waitress import serve
     serve(app, host='0.0.0.0', port='7050')
