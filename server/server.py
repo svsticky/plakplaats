9e621c0e6c21017da558ad849cfd7e980af199a9
ArondenOuden <50593451+ArondenOuden@users.noreply.github.com>
2026-02-09 23:59:16 +0100

    Fix: Typo in server.py
    Co-authored-by: Noah van der Aa <ndvdaa@gmail.com>

--- server/server.py
+++ server/server.py
@@ -413,7 +413,7 @@ def reviewSticker():
         if not sticker:
             return jsonify({'error': 'Sticker not found'}), 404
         sticker.reviewed = True
-        session.verified = approved
+        sticker.verified = approved
         session.commit()
         return jsonify({'status': 'ok'}), 200
 
